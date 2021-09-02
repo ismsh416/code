9df8983c392bc8b3358605428362796b1b810450
ismsh416 <ismailshaik0416@gmail.com>
2021-09-03 05:23:08 +0530

    Five

--- September/src/com/Test4.java
+++ September/src/com/Test4.java
@@ -3,8 +3,12 @@ package com;
 public class Test4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println(2.0/0.0);
+	System.out.println(0.0/0.0);
 
 	}
 
 }
+
+
+//To check infinity and NaN-Not A numbers
