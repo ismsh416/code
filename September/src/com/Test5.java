8da5b40173d35e1d71885c7d423c0aa7bdf64155
ismsh416 <ismailshaik0416@gmail.com>
2021-09-03 11:30:54 +0530

    five

--- September/src/com/Test5.java
+++ September/src/com/Test5.java
@@ -2,4 +2,24 @@ package com;
 
 public class Test5 {
 
+	//To Print ASCI value of a string
+		public static void main(String[] args) {
+			String S = "BBASSSRPPPOCCBdcbaa";
+			for (int i = 0; i < S.length(); i++) {
+				char c = S.charAt(i);
+				int asc = (int) c;
+				System.out.println(c + "=" + asc);
+
+			}
+//printing ASCI from A-z
+			char k1 = 'A';
+			char k2 = 'z';
+			System.out.println("Printing from "+k1+" to "+k2);
+		
+			for (int i = (int) k1; i <= (int) k2; i++) {
+				System.out.println((char) i + "=" + i);
+			}
+
+		}
+	
 }
