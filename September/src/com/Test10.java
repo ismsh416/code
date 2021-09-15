f24be6feef9923e2d4a3838a81d6a3481ff517de
ismsh416 <ismailshaik0416@gmail.com>
2021-09-15 19:55:17 +0530

    DONE

---
+++ September/src/com/Test10.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test10 {
+
+	public static void main(String[] args) {
+		int[] ar1= {9,9,1,2,7};
+		int[] ar2= {8,7,4,3,6};
+		int len=(ar1.length)+(ar2.length);
+		System.out.println(len);
+		Set<Integer> s=new HashSet<>();
+		System.out.println(s);
+
+	}
+
+}
--- September/src/com/Test9.java
+++ September/src/com/Test9.java
@@ -1,5 +1,5 @@
 package com;
-
+//URL response code checking
 import java.net.HttpURLConnection;
 import java.net.URL;
 
