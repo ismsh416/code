ec74b63c2e1d5d98a729e7b6b271d0aa1de1d17e
ismsh416 <ismailshaik0416@gmail.com>
2021-09-21 08:01:24 +0530

    remove spaces of string

--- September/src/com/Test15.java
+++ September/src/com/Test15.java
@@ -6,6 +6,8 @@ public class Test15 {
 	public static void main(String[] args) {
 		String Str = "hello world this is java program";
 		String[] s = Str.split(" ");
+		System.out.println("input :"+Str);
+		System.out.print("output :");
 		for (String temp : s) {
 			System.out.print(temp);
 		}
