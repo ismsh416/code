c49c3d071504d38c68a30043389c697c7a8ca449
ismsh416 <ismailshaik0416@gmail.com>
2021-09-21 08:32:33 +0530

    sorting and removing duplicates

--- September/src/com/Test10.java
+++ September/src/com/Test10.java
@@ -1,18 +1,29 @@
 package com;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-
+//sorting and removing duplicates
+//for extra i have opted arraycopy to merge array instead two for loops
 public class Test10 {
 
 	public static void main(String[] args) {
 		int[] ar1= {9,9,1,2,7};
 		int[] ar2= {8,7,4,3,6};
-		int len=(ar1.length)+(ar2.length);
-		System.out.println(len);
-		Set<Integer> s=new HashSet<>();
-		System.out.println(s);
-
+		int[] ar3=new int[ar1.length+ar2.length];
+		//Merging both arrays to single array
+		System.arraycopy(ar1,0,ar3,0,ar1.length);
+		System.arraycopy(ar2,0,ar3,ar1.length,ar2.length);
+		System.out.println(Arrays.toString(ar3));
+	
+		Set<Integer> set=new HashSet<>();
+		for(int i=0;i<=ar3.length-1;i++)
+		{
+			set.add(ar3[i]);
+			
+		}
+		
+		System.out.println(set);
 	}
 
 }
