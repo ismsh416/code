b8f8783f3d6d51cf4c19d74ba79153af98abc857
ismsh416 <ismailshaik0416@gmail.com>
2021-09-21 08:02:58 +0530

    duplicate character finder in string

--- September/src/com/Test16.java
+++ September/src/com/Test16.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Test16 {
 
 	public static void main(String[] args) {
-		String str = "MADAM";
+		String str = "HELLOWORLD";
 		char[] ch = str.toCharArray();
 		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
 		for (int i = 0; i <= ch.length - 1; i++) {
